ad63ae47e8fac7c1fbe0dee8bb38aaf2f6ed74b4
Bjorn Hagstrom <bjorn.h@think-cc.com>
2018-08-24 11:29:56 -0500

    let images and code break containers

--- template-parts/content.php
+++ template-parts/content.php
@@ -13,8 +13,11 @@
 	<header class="entry-header">
 		<div class="small-container">
 		<?php
+		$min=0;
+		$max=5;
+		$title_class = 'style' . rand($min,$max);
 		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+			the_title( '<h1 id="title" class="'.$title_class.'">', '</h1>' ); 
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
